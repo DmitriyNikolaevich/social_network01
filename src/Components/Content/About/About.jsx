ec20635f9d3d23ca664027a77e893efa1adcd6ae
DmitriyNikolaevich <dnkaplun1987@gmail.com>
2020-10-09 00:19:45 +0300

    Continu create a structure

--- src/Components/Content/About/About.jsx
+++ src/Components/Content/About/About.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Friends from '../Friends/Friends';
+import Photos from '../Photos/Photos';
 import s from './About.module.css';
+import FavoriteBooks from './FavoriteBooks/FavoriteBooks';
+import FavoriteSports from './FavoriteSports/FavoriteSports';
+import Variants from './Variants/Variants';
 
 const About = (props) => {
     return (
         <div>
-            About
+            <Variants />
+            <Photos />
+            <Friends />
+            <FavoriteBooks />
+            <FavoriteSports />
         </div>
     )
 }
